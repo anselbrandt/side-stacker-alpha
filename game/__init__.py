f781f618485f288b9e64e34c90a6326800d27ff0
anselbrandt <ansel@anselbrandt.com>
2025-03-19 15:51:32 -0700

    Update notebook

--- game/__init__.py
+++ game/__init__.py
@@ -1,8 +1,9 @@
 from .engine import filter_central, get_optimal
-from .logic import new_board, next_player, valid, win_condition
+from .logic import Board, new_board, next_player, valid, win_condition
 from .sim import display_likelihood, game_updater
 
 __all__ = [
+    "Board",
     "display_likelihood",
     "filter_central",
     "game_updater",
